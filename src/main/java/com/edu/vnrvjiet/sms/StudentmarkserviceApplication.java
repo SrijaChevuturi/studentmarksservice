0f96c5e8b60a2d21773783f9a2ecd799d513e986
Srija Chevuturi <srija.chevuturi@gmail.com>
2025-11-14 12:24:27 +0530

    added comment in StudentmarksserviceApplication

--- src/main/java/com/edu/vnrvjiet/sms/StudentmarkserviceApplication.java
+++ src/main/java/com/edu/vnrvjiet/sms/StudentmarkserviceApplication.java
@@ -8,6 +8,7 @@ public class StudentmarkserviceApplication {
 
 	public static void main(String[] args) {
         //sample
+        //added comment
 		SpringApplication.run(StudentmarkserviceApplication.class, args);
         System.out.println("Tomcat running for Student Mark Microservice...");
 	}
